1d3ba8575e3c15591194048d65349a398573ab41
logerangarovic-svg <mihailmkkarenko@gmail.com>
2026-01-14 20:19:02 +0500

    добавил мяч

--- pinpong/pinpong.py
+++ pinpong/pinpong.py
@@ -18,6 +18,26 @@ class Player():
             self.hitbox.top = 0
 player1 = Player(200, 40,1)
 
+class Ball():
+    def __init__(self, x, y, speed):
+        self.hitbox = Rect(x, y, 34, 34)
+        self.speed = speed
+        self.speed_x = speed
+        self.speed_y = speed  
+    def move(self):
+        self.hitbox.x += self.speed_x
+        self.hitbox.y += self.speed_y
+        if self.hitbox.bottom > 480:
+            self.speed_y *= -1
+        if self.hitbox.top < 0:
+            self.speed_y *= -1
+        if self.hitbox.left < 0:
+            self.speed_x *= -1
+        if self.hitbox.right > 720:
+            self.speed_x *= -1
+ball1 = Ball(180,42,10)
+
+
 
 while True:
     win.fill((0, 0, 0))
@@ -27,6 +47,12 @@ while True:
             exit()
     player1.move()
     draw.rect(win, (255, 0, 0), player1.hitbox)
+    ball1.move()
+    
+    if player1.hitbox.colliderect(ball1.hitbox):
+        ball1.speed_x *= -1  
+    draw.rect(win, (255, 200, 0), ball1.hitbox)
+
 
     clock.tick(120)
     display.update()
